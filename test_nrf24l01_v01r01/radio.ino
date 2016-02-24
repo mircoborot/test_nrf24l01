ef1c2840383ed96fdc7e47c9472b382b7f351ce6
mircoborot <antonio.curci.it@gmail.com>
2016-02-25 00:13:00 +0100

    Added files via upload

---
+++ test_nrf24l01_v01r01/radio.ino
@@ -0,0 +1,71 @@
+/* ---------------------------------------------------------
+Package     :  (·K) Leonardo v.1 (R)
+Module      :  radio
+Copyright   :  (c) 2014 by Antonio Curci 
+
+Copyright 2014 by Antonio Curci. All rights reserved.
+Redistribution and use in source and binary forms,
+with or without modification, are not permitted.
+
+-------------------------------------------------------- */
+
+// ******************** LIBRARIES
+#include "printf.h"  // serve per la stampa iniziale di radio.printDetails();
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+
+// ******************** DEFINES
+#define PORT_TX  0xDEDEDEDEE7LL
+#define PORT_RX  0xDEDEDEDEE9LL
+
+// ******************** CONSTANTS
+// PIN
+const byte PIN_RAD_CE  = 8, PIN_RAD_CSN = 10;  // Arduino UNO
+// const byte PIN_RAD_CE  = 46, PIN_RAD_CSN = 47;  // Arduino MEGA
+  
+// ******************** VARIABLES
+RF24 radio(PIN_RAD_CE,PIN_RAD_CSN);    // RF24 radio(CE,CSN);
+
+// ******************** FUNCTIONS
+void radsetup() {
+  printf_begin();
+  radio.begin();
+  
+  radio.setDataRate(RF24_250KBPS);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setChannel(70);
+  
+  radio.enableDynamicPayloads();
+  radio.setRetries(15,15);
+  radio.setCRCLength(RF24_CRC_16);
+
+  radio.openWritingPipe(PORT_TX);
+  radio.openReadingPipe(1,PORT_RX);  
+  
+  radio.startListening();
+  radio.printDetails();
+}
+
+boolean radav()     {return radio.available();}
+byte radinlen()     {return radio.getDynamicPayloadSize();}
+boolean radin(void* p, int n)    {return radio.read(p,n);}
+
+boolean radout(void* p, int n) {
+  // swap TX & Rx addr for writing
+  radio.openWritingPipe(PORT_RX);
+  radio.openReadingPipe(0,PORT_TX);  
+  radio.stopListening();
+        
+// bool ok = radio.write(&SendPayload,strlen(SendPayload));
+  bool ok = radio.write(p,n);
+        
+  // restore TX & Rx addr for reading       
+  radio.openWritingPipe(PORT_TX);
+  radio.openReadingPipe(1,PORT_RX); 
+  radio.startListening();  
+
+  return ok;
+}
+
+
---
+++ test_nrf24l01_v01r01/test_nrf24l01_v01r01.ino
@@ -0,0 +1,123 @@
+/* ---------------------------------------------------------
+Package     :  (·K) Leonardo v.1 (R)
+Module      :  radio
+Copyright   :  (c) 2014 by Antonio Curci 
+
+Copyright 2014 by Antonio Curci. All rights reserved.
+Redistribution and use in source and binary forms,
+with or without modification, are not permitted.
+
+History     :  
+  Version  Description
+    01      original
+    02      senza lcd
+    03      astrazione
+
+-------------------------------------------------------- */
+#define DEBUG_PREF    "\r\n  ###"
+
+#define serout(x)    Serial.print(x)
+
+boolean stringOverflow = false;
+char charOverflow = 0;
+boolean stringComplete = false;  // whether the string is complete
+static int dataBufferIndex = 0;
+
+char serialBuffer[31] = "";
+char RecvPayload[31] = "";
+char SendPayload[31] = "";
+
+void setup(void) {
+ 
+  Serial.begin(9600);
+
+  radsetup();
+
+  Serial.println();
+  Serial.println("RF Chat V0.90");    
+  delay(500);
+}
+
+void loop(void) {radio_receive();serial_receive();} 
+
+void serialEvent() {
+  while (Serial.available() > 0 ) {
+      char incomingByte = Serial.read();
+      
+      if (stringOverflow) {
+         serialBuffer[dataBufferIndex++] = charOverflow;  // Place saved overflow byte into buffer
+         serialBuffer[dataBufferIndex++] = incomingByte;  // saved next byte into next buffer
+         stringOverflow = false;                          // turn overflow flag off
+      } else if (dataBufferIndex > 31) {
+         stringComplete = true;        // Send this buffer out to radio
+         stringOverflow = true;        // trigger the overflow flag
+         charOverflow = incomingByte;  // Saved the overflow byte for next loop
+         dataBufferIndex = 0;          // reset the bufferindex
+         break; 
+      } 
+      else if(incomingByte=='\n' || incomingByte=='\r'){
+          serialBuffer[dataBufferIndex] = 0; 
+          stringComplete = true;
+      } else {
+          serialBuffer[dataBufferIndex++] = incomingByte;
+          serialBuffer[dataBufferIndex] = 0; 
+      }          
+  } 
+} 
+
+void radio_receive(void) {
+  int len = 0;
+  bool done = false;
+  
+  if (radav()) {  
+      while (!done) {
+        len = radinlen();
+        done = radin(&RecvPayload,len);
+        delay(5);
+      }
+  
+    RecvPayload[len] = 0; // null terminate string
+    
+    strout(" radin",RecvPayload,len);
+
+    Serial.print("\r\nR:");
+    Serial.print(RecvPayload);
+    Serial.println();
+    RecvPayload[0] = 0;  // Clear the buffers
+  }  
+
+}
+
+void serial_receive(void){
+  
+  if (stringComplete) { 
+        strcat(SendPayload,serialBuffer);      
+
+        radout(&SendPayload,strlen(SendPayload));
+        
+        strout("radout",SendPayload,strlen(SendPayload));
+        
+        Serial.print("\r\nS:");
+        Serial.print(SendPayload);          
+        Serial.println();
+        stringComplete = false;
+
+        SendPayload[0] = 0;
+        dataBufferIndex = 0;
+  } 
+} 
+
+
+    
+    
+void strout(char *name,char *p,byte n) {
+  serout(DEBUG_PREF);serout(' ');
+  serout(name);
+  serout('[');serout(n);serout("]=<");
+  for(byte i=0;i<n;i++) {echoHex(*(p+i));serout(' ');}
+  serout(">");
+}
+
+char dec2Hex(char x)  {if (0<=x&&x<=9) return '0'+x; else return 'A'+x-10;}
+void echoHex(byte x)  {serout(dec2Hex(x/16));serout(dec2Hex(x%16));} 
+void echoLineHex(char *p) {while (*p) {echoHex(*p);p++;serout(' ');}}
